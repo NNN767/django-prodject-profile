426318c78021a705ccc7bf566aa18b781658b45a
niki <Skarprais@mail.ru>
2024-02-13 18:00:13 +0300

    5.5

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class  Project(models.Model): #класс models позволяет работать и связываться с бозой данных создовать
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/') # upload_to обозначает в какой директории будут сохроняться изображения
+    url = models.URLField(blank=True,)
+
+
