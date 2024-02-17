7ae7781e0aa8adfc4673389fb8486570d5268e46
niki <Skarprais@mail.ru>
2024-02-17 16:49:57 +0300

    5.10

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.urls import path ,include
 from . import views
 
-
+app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog ),
+    path('<int:blog_id>/', views.detail, name = 'detail' ),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import blog as bd_blog
 from .models import person_img
 from portfolio.models import Project
@@ -16,3 +16,7 @@ def blog(request):
     img_lent = Project.objects.all()
 
     return render(request,'blog/home.html', {'test' : test, 'img':img,'img_lent':img_lent})
+
+def detail(request, blog_id ):
+    date = get_object_or_404(bd_blog,pk = blog_id )
+    return render(request,'blog/detail.html',{'id':date})
